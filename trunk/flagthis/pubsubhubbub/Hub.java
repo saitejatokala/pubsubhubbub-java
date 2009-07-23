25cebf953dc52464753fd3e778402cd25e633b53
royans <royans@62b49c7c-6daa-11de-98c1-619de02e93b8>
2009-07-23 05:47:31 +0000

    Added class to Subscribe to feeds. Added class to startup webserver (embedded jetty server) to talk to external web requests.

---
+++ trunk/flagthis/pubsubhubbub/Hub.java
@@ -0,0 +1,5 @@
+package flagthis.pubsubhubbub;
+
+public class Hub {
+
+}
--- trunk/flagthis/test/Test.java
+++ trunk/flagthis/test/Test.java
@@ -1,29 +1,46 @@
 package flagthis.test;
 
 import java.io.IOException;
-
+import flagthis.pubsubhubbub.Discovery;
 import flagthis.pubsubhubbub.Publisher;
+import flagthis.pubsubhubbub.Subscriber;
+import flagthis.pubsubhubbub.Web;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void testPublisher() throws Exception {
 		Publisher publisher = new Publisher();
 		String hub = "http://pubsubhubbub.appspot.com/publish";
+		int status = publisher
+				.execute(hub,
+						"http://www.royans.net/rant/feed/");
+		System.out.println("Return status : " + status);
+		status = publisher
+				.execute(hub,
+						"http://www.royans.net/rant/feed/");
+
+		status = publisher
+				.execute(hub,
+						"http://www.royans.net/rant/feed/");
+
+		System.out.println("Return status : " + status);
+	}
+
+	public static void testSubscriber() throws Exception {
+		Web webserver = new Web(8080);
+		webserver.start();
+		Subscriber subscriber=new Subscriber(webserver);
+		Thread.sleep(30000);
+		int result=subscriber.subscribe("http://www.royans.net/rant/feed/","http://www.webtrace.info:8080/","rkt_at_pobox.com_is_testing","300");
+		System.out.println("Subscribtion status :"+result);
+	}
+
+	public static void main(String[] args) {
 		try {
-
-			int status = publisher
-					.execute(hub,
-							"http://www.royans.net/rant/2009/07/05/cell-phone-speeds-reliability-in-us/");
-			System.out.println("Return status : " + status);
-			status = publisher
-					.execute(hub,
-							"http://www.royans.net/rant/2009/07/05/cell-phone-speeds-reliability-in-us/");
-
-			status = publisher
-			.execute(hub,
-					"http://www.royans.net/rant/2009/07/05/cell-phone-speeds-reliability-in-us/");
-
-			System.out.println("Return status : " + status);
+			//testPublisher();
+			testSubscriber();
+			//Discovery discovery=new Discovery();
+			//System.out.println(discovery.getHub("http://www.royans.net/rant/feed/"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
