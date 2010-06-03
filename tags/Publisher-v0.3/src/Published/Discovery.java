8b81d258119c771fa7fe31a7fe680615696f1f4d
OscNovo <OscNovo@62b49c7c-6daa-11de-98c1-619de02e93b8>
2010-06-03 10:58:18 +0000

    cleaning the folders

--- tags/Publisher-v0.3/src/Published/Discovery.java
+++ tags/Publisher-v0.3/src/Published/Discovery.java
@@ -1,4 +1,4 @@
-package PubSubHubbub;
+package Published;
 
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -20,8 +20,8 @@ import javax.xml.xpath.*;
 public class Discovery {
 
 	public Discovery() {
-	}
-
+	} 
+	
 	public String hasHub(Document doc) throws Exception {
 		String hub = null;
 		
@@ -32,12 +32,12 @@ public class Discovery {
 		try{
 			xPathExpression = xPath.compile("/feed/link[@rel='hub']/@href");
 			hub = (String) xPathExpression.evaluate(doc);
-			if ((hub==null)||(hub.equals(""))){
+			if ((hub==null)||(hub=="")){
 				xPathExpression = xPath.compile("//link[@rel='hub']/@href");
 				hub = (String) xPathExpression.evaluate(doc);			
 			}	
 			
-			if (hub.equals("")){
+			if (hub ==""){
 				return null;
 			}
 			
@@ -76,7 +76,6 @@ public class Discovery {
 	
 	public String getContents(String feed) throws Exception {
 		String response = null;
-		
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpGet httpget = new HttpGet(feed);
 		ResponseHandler<String> responseHandler = new BasicResponseHandler();
@@ -89,14 +88,22 @@ public class Discovery {
 	}
 
 	public String getHub(String feedurl) throws Exception {
-		
 		DocumentBuilderFactory Factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = Factory.newDocumentBuilder();
 		Document doc = builder.parse(new InputSource(new StringReader(
 				getContents(feedurl))));
-		
-		return (hasHub(doc));
-		
+
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xPath = factory.newXPath();
+		XPathExpression xPathExpression;
+		String hub;
+		xPathExpression = xPath.compile("/feed/link[@rel='hub']/@href");
+		hub = (String) xPathExpression.evaluate(doc);
+		if ((hub==null)||(hub.equals(""))){
+			xPathExpression = xPath.compile("//link[@rel='hub']/@href");
+			hub = (String) xPathExpression.evaluate(doc);			
+		}
+		return hub;
 	}
 
 	public HashMap<String, String> getHubs(ArrayList<String> feedurls) {
